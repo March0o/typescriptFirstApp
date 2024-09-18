cd69188b56b42dc0efdc15e452167e2a631011a2
MC <martynascep9@gmail.com>
2024-09-18 10:26:45 +0100

    1st Commit

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import express, {Application, Request, Response} from "express"; 
+import morgan from "morgan";
+import userRoutes from './routes/users'; 
+ 
+const PORT = process.env.PORT || 3000; 
+
+const app: Application = express(); 
+
+app.use(morgan("tiny")); 
+
+app.use('/api/v1/users', userRoutes) 
+ 
+app.get("/ping", async (_req : Request, res: Response) => { 
+
+ res.send({ 
+
+ message: "hello from Una, dee do do", 
+
+ }); 
+}); 
+
+app.get("/bananas", async (_req : Request, res: Response) => { 
+
+    res.send({ 
+   
+    message: "THIS IS BANANAS", 
+   
+    }); 
+   }); 
+
+   app.get("/apples", async (_req : Request, res: Response) => { 
+
+    res.send({ 
+   
+    message: "THIS IS APPLES", 
+   
+    }); 
+   });  
+
+   app.get("/music", async (_req : Request, res: Response) => { 
+
+    res.send({ 
+   
+    message: "sitting on the dock of the bay", 
+   
+    }); 
+   });  
+
+ app.listen(PORT, () => { 
+
+    console.log("Server is running on port  --", PORT); 
+
+    }); 
